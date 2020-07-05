944a3520eddc89ef4fae12ef45e68d6feae11994
michaelcanyon <ucheba0536@outlook.com>
2020-07-05 12:50:03 +0300

    Проект в отдельный репозиторий

---
+++ FractalSets/FractalDrawer/IFractalDrawer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace FractalSets
+{
+    public interface IFractalDrawer
+    {
+
+        /// <summary>
+        /// Отрисовывает фрактал
+        /// </summary>
+        /// <param name="fractalParam">реализация фрактала</param>
+        /// <returns>Заполненная картинка</returns>
+        Bitmap Draw(IFractalBase fractalParam);
+    }
+}
---
+++ FractalSets/IPicture.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+namespace FractalSets
+{
+    public interface IPicture
+    {
+        int Width { get; set; }
+        int Height { get; set; }
+        Bitmap Draw();
+    }
+
+}
